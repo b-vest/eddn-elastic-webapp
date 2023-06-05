a013da3bc1766d8ba0bb213a7df18c4e8d454f18
Bryan Vest <murlin99@gmail.com>
2023-06-05 18:51:08 +0000

    Basic websocket working

--- public/js/site.js
+++ public/js/site.js
@@ -1,8 +1,28 @@
-  function responsive() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+const socket = new WebSocket('ws://172.16.1.252:3000');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+  console.log('WebSocket connected');
+  // You can send data to the server using socket.send()
+  socket.send('Hello Server!');
+});
+
+// Listen for messages from the server
+socket.addEventListener('message', function (event) {
+  console.log('Message from server:', event.data);
+});
+
+// Connection closed
+socket.addEventListener('close', function (event) {
+  console.log('WebSocket connection closed');
+});
+
+
+function responsive() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
