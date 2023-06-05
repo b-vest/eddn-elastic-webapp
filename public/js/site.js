9b756c250ed1a2be2b1f1f15ab4dd3765508f9dc
Bryan Vest <murlin99@gmail.com>
2023-06-05 20:14:48 +0000

    Cleaned up node status updates that come from the websocket

--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,14 @@
+
+//Key List for Cluster Health Items to Print
+const healthKey = {
+	cluster_name: "Cluster Name",
+	status: "Overall Statys",
+	number_of_nodes: "# Nodes",
+	number_of_data_nodes: "# Data Nodes",
+	unassigned_shards: "# Unassigned",
+	number_of_pending_tasks: "# Pending Tasks"
+}
+
 const socket = new WebSocket('ws://172.16.1.252:3000');
 
 // Connection opened
@@ -14,17 +25,23 @@ socket.addEventListener('message', function (event) {
 const jsonData = JSON.parse(event.data);
 const healthData = jsonData.health;
 for (const key in healthData) {
-  const row = table.insertRow();
-  const cell1 = row.insertCell();
-  const cell2 = row.insertCell();
-  cell1.textContent = key;
-  cell2.textContent = healthData[key];
-  console.log(key);
+	if(healthKey[key]){
+  		const row = table.insertRow();
+  		const cell1 = row.insertCell();
+  		const cell2 = row.insertCell();
+  		cell1.textContent = healthKey[key];
+  		cell2.textContent = healthData[key];
+  		console.log(key);
+  	}
 }
+d = new Date();
+
+
 
 const nodeStatusCol = document.getElementById('nodeStatusCol');
 nodeStatusCol.innerHTML = "";
 nodeStatusCol.appendChild(table);
+nodeStatusCol.innerHTML += "<p>"+d.toLocaleString()+"<\p>";
 });
 
 // Connection closed
