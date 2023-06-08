ab9fd48fcd4425b8727ae5c6b5df02a5785f4de8
Bryan Vest <murlin99@gmail.com>
2023-06-08 01:15:33 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
